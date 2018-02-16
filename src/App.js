21e42ba937b34211c9bc9518eabdac75df2bc5e7
Luis Garzon <luisgarzon90@yahoo.com>
2018-02-15 19:39:14 -0500

    added people and loading stage additionally created a ordered list with star wars characters

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
+const BASE_URL = "https://swapi.co/api/";
+const PEOPLE_URL = `${BASE_URL}people`
+
+
+    
+
 class App extends Component {
+  constructor(){
+    super(); 
+    this.state = {
+      people: [ ],
+      loading: true 
+    }
+  }
+  componentDidMount(){
+    console.log('componentDidMount')
+    axios.get(PEOPLE_URL)
+    .then((response) => this.setState({loading: false, people: response.data.results}))
+    .catch((err) => console.log(err));
+  }
   render() {
+    if(this.state.loading){
+      return <h1>Loading...</h1>
+    }
+    const people = this.state.people.map(person => <li>{person.name}</li>);
     return (
-     <div> 
-       Hello
-      </div>
+     <ul>
+       {people}
+     </ul>
     );
   }
 }
