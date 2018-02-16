cb64d6fd53543bbe9812c28f4814428316f8b210
Luis Garzon <luisgarzon90@yahoo.com>
2018-02-15 20:35:49 -0500

    created star wars app that switch from people to planets from the push of a button

--- src/App.js
+++ src/App.js
@@ -1,36 +1,64 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import './App.css';
+
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
 
 const BASE_URL = "https://swapi.co/api/";
-const PEOPLE_URL = `${BASE_URL}people`
-
-
-    
+const PEOPLE_URL = `${BASE_URL}people`;
+const PLANET_URL = `${BASE_URL}planets`;
 
 class App extends Component {
-  constructor(){
-    super(); 
+  constructor() {
+    super();
     this.state = {
-      people: [ ],
-      loading: true 
-    }
+      people: [],
+      planets: [],
+      loading: true
+    };
+    this.fetchPlanets = this.fetchPlanets.bind(this);
+    this.fetchPeople = this.fetchPeople.bind(this);
   }
-  componentDidMount(){
-    console.log('componentDidMount')
-    axios.get(PEOPLE_URL)
-    .then((response) => this.setState({loading: false, people: response.data.results}))
-    .catch((err) => console.log(err));
+  componentDidMount() {
+    axios
+      .get(PEOPLE_URL)
+      .then(response =>
+        this.setState({ loading: false, people: response.data.results })
+      )
+      .catch(err => console.log(err));
+  }
+  fetchPlanets() {
+    this.setState({ loading: true });
+    axios
+      .get(PLANET_URL)
+      .then(response =>
+        this.setState({ loading: false, planets: response.data.results, people: [] })
+      )
+      .catch(err => console.log(err));
+  }
+  fetchPeople(){
+    this.setState({ loading: true});
+    axios
+      .get(PEOPLE_URL)
+      .then(response => this.setState({ loading: false, people: response.data.results, planets: [] }))
+      .catch(err => console.log(err));
   }
   render() {
-    if(this.state.loading){
-      return <h1>Loading...</h1>
+    if (this.state.loading) {
+      return <h1>Loading....</h1>;
     }
-    const people = this.state.people.map(person => <li>{person.name}</li>);
+    const planets = this.state.planets.map((planet, index) => (
+      <li key={index}>{planet.name}</li>
+    ));
+    const people = this.state.people.map((person, index) => (
+      <li key={index}>{person.name}</li>
+    ));
     return (
-     <ul>
-       {people}
-     </ul>
+      <div>
+        <ul>{planets.length === 0 ? people : planets}</ul>
+        {planets.length === 0 ? 
+          <button onClick={this.fetchPlanets}>What are the planets?</button> :
+          <button onClick={this.fetchPeople}>What are the people?</button>}
+      </div>
     );
   }
 }
